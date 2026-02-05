758ca94d5f2b743419a233a5f65e5ce0ba81429e
Noor Eldeen Salah <nooreldeensalah99@gmail.com>
2026-02-06 00:03:53 +0200

    Simplify entry points

--- lib/app.dart
+++ lib/app.dart
@@ -31,45 +31,18 @@ class AppShell extends ConsumerStatefulWidget {
   ConsumerState<AppShell> createState() => _AppShellState();
 }
 
-class _AppShellState extends ConsumerState<AppShell>
-    with SingleTickerProviderStateMixin {
+class _AppShellState extends ConsumerState<AppShell> {
   int _selectedIndex = 0;
-  int _previousIndex = 0;
-
-  late final AnimationController _transitionController = AnimationController(
-    vsync: this,
-    duration: const Duration(milliseconds: 300),
-  );
-
-  @override
-  void dispose() {
-    _transitionController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
-    final pages = [
-      for (final route in AppRoute.values)
-        KeyedSubtree(
-          key: PageStorageKey<String>('route:${route.name}'),
-          child: route.buildPage(context, ref),
-        ),
-    ];
-
     return Scaffold(
       body: Row(
         children: [
           NavigationRail(
             selectedIndex: _selectedIndex,
-            onDestinationSelected: (index) {
-              if (index == _selectedIndex) return;
-              setState(() {
-                _previousIndex = _selectedIndex;
-                _selectedIndex = index;
-              });
-              _transitionController.forward(from: 0);
-            },
+            onDestinationSelected: (index) =>
+                setState(() => _selectedIndex = index),
             labelType: NavigationRailLabelType.all,
             leading: Padding(
               padding: const EdgeInsets.symmetric(vertical: 16),
@@ -113,46 +86,15 @@ class _AppShellState extends ConsumerState<AppShell>
           ),
           const VerticalDivider(thickness: 1, width: 1),
           Expanded(
-            child: AnimatedBuilder(
-              animation: _transitionController,
-              builder: (context, _) {
-                final isAnimating = _transitionController.isAnimating;
-
-                double clamp01(double t) => t < 0 ? 0.0 : (t > 1 ? 1.0 : t);
-
-                // Material 3 top-level destinations: fade-through.
-                // Outgoing fades quickly, incoming fades in with a slight delay.
-                final t = _transitionController.value;
-                final outgoingOpacity = isAnimating
-                    ? 1.0 - Curves.easeIn.transform(clamp01(t / 0.35))
-                    : 0.0;
-                final incomingOpacity = isAnimating
-                    ? Curves.easeOut.transform(clamp01((t - 0.15) / 0.85))
-                    : 1.0;
-
-                return Stack(
-                  children: [
-                    for (var i = 0; i < pages.length; i++)
-                      Offstage(
-                        offstage:
-                            !(i == _selectedIndex ||
-                                (isAnimating && i == _previousIndex)),
-                        child: TickerMode(
-                          enabled: i == _selectedIndex,
-                          child: IgnorePointer(
-                            ignoring: i != _selectedIndex,
-                            child: Opacity(
-                              opacity: i == _selectedIndex
-                                  ? incomingOpacity
-                                  : outgoingOpacity,
-                              child: pages[i],
-                            ),
-                          ),
-                        ),
-                      ),
-                  ],
-                );
-              },
+            child: IndexedStack(
+              index: _selectedIndex,
+              children: [
+                for (final route in AppRoute.values)
+                  KeyedSubtree(
+                    key: PageStorageKey('route:${route.name}'),
+                    child: route.buildPage(context, ref),
+                  ),
+              ],
             ),
           ),
         ],
--- lib/main.dart
+++ lib/main.dart
@@ -5,75 +5,21 @@ import 'package:systemd_manager/providers/providers.dart';
 import 'package:systemd_manager/services/services.dart';
 import 'package:ubuntu_logger/ubuntu_logger.dart';
 
-final _log = Logger('main');
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   Logger.setup(level: LogLevel.info);
 
-  _log.info('Starting Systemd Manager');
-
-  _log.info('Initializing services...');
   final dbusService = DBusSystemdService();
   await dbusService.connect();
-
-  final journalService = JournalService();
-  final analyzeService = AnalyzeService();
-
-  _log.info('Services initialized');
 
   runApp(
     ProviderScope(
       overrides: [
         systemdServiceProvider.overrideWithValue(dbusService),
-        journalServiceProvider.overrideWithValue(journalService),
-        analyzeServiceProvider.overrideWithValue(analyzeService),
+        journalServiceProvider.overrideWithValue(JournalService()),
+        analyzeServiceProvider.overrideWithValue(AnalyzeService()),
       ],
-      child: _AppLifecycleManager(
-        onDispose: () async {
-          _log.info('Disposing services...');
-          await dbusService.dispose();
-          _log.info('Services disposed');
-        },
-        child: const SystemdManagerApp(),
-      ),
+      child: const SystemdManagerApp(),
     ),
   );
-}
-
-class _AppLifecycleManager extends StatefulWidget {
-  const _AppLifecycleManager({required this.child, required this.onDispose});
-
-  final Widget child;
-  final Future<void> Function() onDispose;
-
-  @override
-  State<_AppLifecycleManager> createState() => _AppLifecycleManagerState();
-}
-
-class _AppLifecycleManagerState extends State<_AppLifecycleManager>
-    with WidgetsBindingObserver {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-  }
-
-  @override
-  void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-    widget.onDispose();
-    super.dispose();
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.detached) {
-      widget.onDispose();
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) => widget.child;
 }
